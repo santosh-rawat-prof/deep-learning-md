11bf775ddf19949b9a963211bc9b866ebf43473b
Prasanta Roy <prasanta37029@gmail.com>
2025-06-01 22:39:14 +0530

    Add Test API

--- backend/src/main/java/com/backend/controller/HomeController.java
+++ backend/src/main/java/com/backend/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.backend.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HomeController {
 
-    @GetMapping("/predict")
+    @PostMapping("/predict")
     public ResponseEntity<?> classify() {
         return new ResponseEntity<>("Simple response", HttpStatus.OK);
+    }
+
+    /* Test your frontend using this API */
+    @GetMapping("/test")
+    public ResponseEntity<?> testApi() {
+        return new ResponseEntity<>("This is test response from server", HttpStatus.OK);
     }
     
 }
