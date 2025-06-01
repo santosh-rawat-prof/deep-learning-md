9f136ce52b34be36187fe5f8860b72f94c574da8
Prasanta Roy <prasanta37029@gmail.com>
2025-06-01 23:00:01 +0530

    Update Result Model

--- backend/src/main/java/com/backend/model/Result.java
+++ backend/src/main/java/com/backend/model/Result.java
@@ -1,14 +1,42 @@
 package com.backend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Result {
     private String equipment;
     private String brand;
     private String confidence;
+
+    public Result(String equipment, String brand, String confidence) {
+        this.equipment = equipment;
+        this.brand = brand;
+        this.confidence = confidence;
+    }
+
+    public String getEquipment() {
+        return equipment;
+    }
+
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(String confidence) {
+        this.confidence = confidence;
+    }
+
+    @Override
+    public String toString() {
+        return "Result [equipment=" + equipment + ", brand=" + brand + ", confidence=" + confidence + "]";
+    }
 }
