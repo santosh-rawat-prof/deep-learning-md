26c05254d3f36f0f9d26a3a8d68d23aefb370c17
Prasanta Roy <prasanta37029@gmail.com>
2025-06-03 20:46:28 +0530

    Create ai-ml backend part and ready to load the model

---
+++ ai-ml/main.py
@@ -0,0 +1,50 @@
+# Run command: 
+# uvicorn main:app --host 0.0.0.0 --port 8000
+
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+import io
+from torchvision import models
+import torch.nn as nn
+
+class_labels = ["Scalpel", "Forceps", "Retractor", "Scissors"]
+
+def load_model():
+    model = models.resnet18(weights=None)
+    model.fc = nn.Linear(512, 4)  # Replace 10 with your class count
+    model.load_state_dict(torch.load("surgical_model.pth", map_location="cpu"))
+    model.eval()
+    return model
+
+model = load_model()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    input_tensor = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        outputs = model(input_tensor)
+        predicted_class_idx = torch.argmax(outputs, dim=1).item()
+        predicted_class_name = class_labels[predicted_class_idx]
+
+    return {"prediction": predicted_class_name}
